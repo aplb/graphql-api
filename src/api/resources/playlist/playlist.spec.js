e660538b4238b46a5033810279c3df0708e507c1
Alex Bashkatov <al7bashkatov@gmail.com>
2019-01-15 21:25:37 +0200

    specs added

--- src/api/resources/playlist/playlist.spec.js
+++ src/api/resources/playlist/playlist.spec.js
@@ -1,8 +1,92 @@
+import { expect } from 'chai'
 import createApiSpec from '~/apiSpecs'
+import { runQuery, dropDb } from '../../../../test/helpers'
 import { Playlist } from './playlist.model'
+import { Song } from '../song/song.model'
 
 createApiSpec(
   Playlist,
   'playlist',
   {title: 'study jams', favorite: true}
 )
+
+describe.only('Playlist', () => {
+  let playlist
+  beforeEach(async () => {
+    await dropDb()
+    playlist = await Playlist.create({ title: 'study jams', favorite: true })
+  })
+
+  afterEach(async () => {
+    await dropDb()
+  })
+
+  it('should create new playlist', async () => {
+    const input = {
+      title: 'test title',
+      favorite: true,
+    }
+
+    const result = await runQuery(`
+      mutation newPlaylist($input: NewPlaylist!) {
+        newPlaylist(input: $input) {
+          title
+          favorite
+        }
+      }
+    `, { input })
+
+    expect(result.errors).to.not.exist
+    expect(result.data.newPlaylist).to.be.an('object')
+    expect(result.data.newPlaylist.title).to.eql(input.title)
+    expect(result.data.newPlaylist.favorite).to.eql(input.favorite)
+  })
+
+  it('should get single playlist', async () => {
+    const result = await runQuery(`
+      {
+        Playlist(id: "${playlist.id}") {
+          id
+          title
+          favorite
+        }
+      }
+    `)
+
+    expect(result.errors).to.not.exist
+    expect(result.data.Playlist).to.be.an('object')
+    expect(result.data.Playlist.id).to.eql(playlist.id)
+    expect(result.data.Playlist.title).to.eql(playlist.title)
+    expect(result.data.Playlist.favorite).to.eql(playlist.favorite)
+  })
+
+  it('should update playlist', async () => {
+    const song = await Song.create({title: 'downtown jamming', url: 'http://music.mp3'})
+    const input = {
+      id: playlist.id,
+      title: 'updated title',
+      favorite: false,
+      songs: [song.id],
+    }
+
+    const result = await runQuery(`
+      mutation updatePlaylist($input: UpdatedPlaylist!) {
+        updatePlaylist(input: $input) {
+          id
+          title
+          favorite
+          songs {
+            id
+          }
+        }
+      }
+    `, { input })
+
+    expect(result.errors).to.not.exist
+    expect(result.data.updatePlaylist).to.be.an('object')
+    expect(result.data.updatePlaylist.id).to.eql(input.id)
+    expect(result.data.updatePlaylist.title).to.eql(input.title)
+    expect(result.data.updatePlaylist.favorite).to.eql(input.favorite)
+    expect(result.data.updatePlaylist.songs[0].id).to.eql(song.id)
+  })
+})
--- src/api/resources/song/song.spec.js
+++ src/api/resources/song/song.spec.js
@@ -1,4 +1,7 @@
+import { expect } from 'chai'
+import merge from 'lodash.merge'
 import createApiSpec from '~/apiSpecs'
+import { runQuery, dropDb } from '../../../../test/helpers'
 import { Song } from './song.model'
 
 createApiSpec(
@@ -6,3 +9,84 @@ createApiSpec(
   'song',
   {title: 'downtown jamming', url: 'http://music.mp3'}
 )
+
+describe.only('Song', () => {
+  let song
+  beforeEach(async () => {
+    await dropDb()
+    song = await Song.create({title: 'downtown jamming', url: 'http://music.mp3'})
+  })
+
+  afterEach(async () => {
+    await dropDb()
+  })
+
+  it('should create new song', async () => {
+    const input = {
+      title: 'test title',
+      url: 'https://example.com',
+    }
+
+    const result = await runQuery(`
+      mutation createSong($input: NewSong!) {
+        newSong(input: $input) {
+          title
+          url
+        }
+      }
+    `, { input })
+
+    expect(result.errors).to.not.exist
+    expect(result.data.newSong).to.be.an('object')
+    expect(result.data.newSong.title).to.eql(input.title)
+    expect(result.data.newSong.url).to.eql(input.url)
+  })
+
+  it('should get all songs', async () => {
+    const result = await runQuery(`
+      {
+        allSongs {
+          title
+          url
+        }
+      }
+    `)
+
+    expect(result.errors).to.not.exist
+    expect(result.data.allSongs).to.be.an('array')
+    expect(result.data.allSongs[0].title).to.eql(song.title)
+    expect(result.data.allSongs[0].url).to.eql(song.url)
+  })
+
+  it('should update a song', async () => {
+    const input = {
+      title: 'test title',
+      url: 'https://example.com',
+    }
+
+    const allSongs = await runQuery(`
+      {
+        allSongs {
+          id
+        }
+      }
+    `)
+    const targetSong = allSongs.data.allSongs[0]
+
+    const result = await runQuery(`
+      mutation updateSong($input: UpdatedSong!) {
+        updateSong(input: $input) {
+          id
+          title
+          url
+        }
+      }
+    `, { input: merge(input, { id: targetSong.id }) })
+
+    expect(result.errors).to.not.exist
+    expect(result.data.updateSong).to.be.an('object')
+    expect(result.data.updateSong.id).to.eql(targetSong.id)
+    expect(result.data.updateSong.title).to.eql(input.title)
+    expect(result.data.updateSong.url).to.eql(input.url)
+  })
+})
